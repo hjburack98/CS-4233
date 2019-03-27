bb66d681572727f13e5f9362b4e9ac6d0b9c7413
hjburack98 <40404459+hjburack98@users.noreply.github.com>
2019-03-27 00:27:45 -0400

    Added TDD project

---
+++ CS 4233 OOAD/OCR-hjburack/src/ocr/OCRTranslator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Copyright ©2016-2017 Gary F. Pollice
+ *******************************************************************************/
+package ocr;
+
+/**
+ * This class has a single method that will translate OCR digits to a string of
+ * text digits that correspond to the OCR digits.
+ * 
+ * @version Mar 13, 2019
+ */
+public class OCRTranslator
+{
+	/**
+	 * Default constructor. You may not add parameters to this. This is
+	 * the only constructor for this class and is what the master tests will use.
+	 */
+	public OCRTranslator()
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Translate a string of OCR digits to a corresponding string of text
+	 * digits. OCR digits are represented as three rows of characters (|, _, and space).
+	 * @param s1 the top row of the OCR input
+	 * @param s2 the middle row of the OCR input
+	 * @param s3 the third row of the OCR input
+	 * @return a String containing the digits corresponding
+	 */
+	public String translate(String s1, String s2, String s3)
+	{
+		// To be implemented
+		return null;
+	}
+}
---
+++ CS 4233 OOAD/OCR-hjburack/test/ocr/OCRTranslatorTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * This files was developed for CS4233: Object-Oriented Analysis & Design.
+ * The course was taken at Worcester Polytechnic Institute.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Copyright ©2016-2017 Gary F. Pollice
+ *******************************************************************************/
+package ocr;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the OCR translator.
+ * @version Mar 13, 2019
+ */
+class OCRTranslatorTest
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
